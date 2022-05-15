537b3ce2dd482572acf83d1a15cbc541d6f51083
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-15 17:54:45 -0300

    ejercicio suma de numeros

--- Suma-entre-dos-numeros/index.ts
+++ Suma-entre-dos-numeros/index.ts
@@ -2,14 +2,14 @@ let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
 let rotulo1 = document.getElementById("rotulo1");
 let rotulo2 = document.getElementById("rotulo2");
-
+let btnEnviar = document.getElementById("enviar");
 rotulo1.innerHTML = "Ingrese primer numero";
 rotulo2.innerHTML = "Ingrese segundo numero";
-
+btnEnviar.addEventListener("click", () => {
 let nro1: number = Number(dato1.value);
 let nro2: number = Number(dato2.value);
 let actual: number;
-let suma: number;
+let suma: number; 
 actual = nro1;
 suma = 0;
 
