46849f29ddcab0c2e1bf7b058d543e3b7094532d
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-15 11:28:37 -0300

    Ejercicio multiplos con el tipeo no funciono, debí copiar y pegar

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-let nro: number;
-let multiplos: string = "";
-
-for (let nro: number = 1; nro <= 100; nro++) {
-  if (nro % 2 == 0 || nro % 3 == 0) {
-    multiplos = multiplos + nro + " ";
-  }
-}
-console.log(multiplos);
